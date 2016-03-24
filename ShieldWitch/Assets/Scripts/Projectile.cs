4c4aa8f02756e03cf82c809506ae0f1f2ed9d5f0
Tevin Meredith <tevin.meredith@gmail.com>
2016-03-24 19:04:19 -0400

    Shield Slows Bullet
    The Shield when put in a front of a bullet will lower the speed and once
    out of the shield will immediately hit the player.

--- ShieldWitch/Assets/Scripts/Projectile.cs
+++ ShieldWitch/Assets/Scripts/Projectile.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Projectile : MonoBehaviour {
 	private Rigidbody2D body2D;
 	public int paceDirection = 1;
+    public float speed = -10f;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,7 +15,8 @@ public class Projectile : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		body2D.AddForce (transform.right * -10 * paceDirection);
+        body2D.velocity = new Vector2(speed, 0);
+		//body2D.AddForce (transform.right * speed * paceDirection);
 		//this.GetComponent<Rigidbody>().AddForce (this.transform.forward * 1000);
 	}
 
@@ -23,8 +25,22 @@ public class Projectile : MonoBehaviour {
 	
 	}
 
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
-        Destroy(gameObject);
+        //Destroy(gameObject);
+        if(col.gameObject.tag == "MagicShield")
+        {
+            //Destroy(gameObject);
+            //body2D.AddForce(transform.right * -.00005f * paceDirection);
+            speed = -.1f;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D col)
+    {
+        if(col.gameObject.tag == "MagicShield")
+        {
+            speed = -10f;
+        }
     }
 }
