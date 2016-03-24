1582f8987975142dd9e5e6744ed63bbc9a2eb44b
makldiz94 <michael.diaz029@knights.ucf.edu>
2016-03-23 23:06:42 -0400

    shield pulse test

--- ShieldWitch/Assets/Scripts/ShieldPulse.cs
+++ ShieldWitch/Assets/Scripts/ShieldPulse.cs
@@ -19,25 +19,23 @@ public class ShieldPulse : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-
-		# if (UNITY_EDITOR || UNITY_WEBPLAYER)
-
-		if (Input.GetButtonDown("Fire1")){
+		if (Input.GetButtonDown("Fire1")){ 
 		Debug.Log("exlosive force");
 		//replace the next line so that objPos1 = location of the player
-		//Vector3 playerPos = transform.position; --> Doesn't work
-		Vector3 objPos1 = new Vector3 (0,0,0);
-		AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, objPos1, Radius);
+		//Vector3 objPos1 = transform.position; //--> Doesn't work
+		
+		//Vector3 objPos1 = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		//Vector3 objPos1 = new Vector3 (0,0,0);
+		Vector3 example = Vector3.zero;
+		AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, example, Radius);
 		}
-		# endif	
-
 	}
 
 	public static void AddExplosionForce (Rigidbody2D body, float expForce, Vector3 expPosition, float expRadius)
 	{
 		var dir = (body.transform.position - expPosition);
 		float calc = 1 - (dir.magnitude / expRadius);
-		if (calc <= 0) {
+		if (calc <= 0) { 
 			calc = 0;		
 		}
 
